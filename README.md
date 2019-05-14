ab602513f01025176cb5bd5f5573909196f5b9d4
mohamadassaf96 <mohammad.assaf@live.com>
2019-05-14 13:33:36 +0200

    Solved list hasCycle. Pushed again.

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ Finds the lowest common ancestor of 2 nodes in a binary tree in O(distance from 
     * unsigned long long swap_bits(unsigned long long x, const int i, const int j): Swaps the ith and jth bits of x.
 * IntervalsUnion.cpp: Given a set of intervals (with endpoints being either open or closed), finds their union. O(nlogn) time and O(1) space where n is the number of intervals.
 * checkBST.cpp: Give the root of a binary tree, checks if the tree satisfies the BST property.
+* hasCycle.cpp: Given a linked list, checks if it has a cycle. O(n) time and O(1) space.
 
 # Notes
 * In all functions, I assumed valid input --checking is not necessary.
---
+++ hasCycle.cpp
@@ -0,0 +1,37 @@
+#include<memory>
+
+template <typename T>
+struct LNode {
+    T data;
+    std::shared_ptr<LNode<T>> next;
+};
+
+template <typename T>
+void insertAfterTail(const std::shared_ptr<LNode<T>>& head, const T& data){
+    std::shared_ptr<LNode<T>> temp = head;
+    while (temp->next) temp = temp->next;
+    std::shared_ptr<LNode<T>> new_node = std::make_shared<LNode<T>>();
+    new_node->data = data;
+    temp->next = new_node;
+}
+
+template <typename T>
+void insertAfterTail(const std::shared_ptr<LNode<T>>& head, const std::shared_ptr<LNode<T>>& new_node){
+    std::shared_ptr<LNode<T>> temp = head;
+    while (temp->next) temp = temp->next;
+    temp->next = new_node;
+}
+
+template <typename T>
+bool hasCycle(const std::shared_ptr<LNode<T>>& head){
+    std::shared_ptr<LNode<T>> turtle = head;
+    std::shared_ptr<LNode<T>> rabbit = head;
+    while (rabbit){
+        turtle = turtle->next;
+        rabbit = rabbit->next ? rabbit->next->next : rabbit ->next;
+        if ((turtle == rabbit) && rabbit){
+            return true;
+        }
+    }
+    return false;
+}
